f1deefd0cdc020fd95fe220bd6d15818562c7482
thrid3v <thridev36@gmail.com>
2025-09-24 15:37:50 +0530

    added landing page

--- my-retro-app/src/components/PixelButton.jsx
+++ my-retro-app/src/components/PixelButton.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 
-const PixelButton = ({ onClick, children, className = '' }) => {
+const PixelButton = ({ label, onClick }) => {
   return (
-    <button
+    <button 
       onClick={onClick}
-      className={`pixel-frame !p-2 bg-[#F28500] hover:bg-[#ff9a21] text-[#0F110C] font-pixel text-sm ${className}`}
+      className="
+        bg-white text-black font-pixel-regular text-2xl px-8 py-2 border-4 
+        border-black rounded-sm
+        hover:bg-retro-pink hover:text-white transition-colors duration-100
+      "
     >
-      <div className="pixel-frame-content !bg-transparent flex items-center justify-center gap-2">
-        {children}
-      </div>
+      {label}
     </button>
   );
 };
